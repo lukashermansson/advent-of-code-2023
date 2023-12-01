c9d9173e230ea377f3d197b760149e0656853b9b
Lukas <lukas@lukashermansson.me>
2023-12-01 20:39:44 +0100

    take a difrent approach

--- day-1/src/bin/part2.rs
+++ day-1/src/bin/part2.rs
@@ -1,6 +1,3 @@
-
-use regex::{Regex};
-
 fn main() {
     let input = include_str!("input.txt").to_string();
     let res: i64 = process(input).iter().sum();
@@ -8,62 +5,60 @@ fn main() {
 }
 
 fn process(input: String) -> Vec<i64> {
+    let regexes = vec![
+        ("1", '1'),
+        ("2", '2'),
+        ("3", '3'),
+        ("4", '4'),
+        ("5", '5'),
+        ("6", '6'),
+        ("7", '7'),
+        ("8", '8'),
+        ("9", '9'),
+        ("one", '1'),
+        ("two", '2'),
+        ("three", '3'),
+        ("four", '4'),
+        ("five", '5'),
+        ("six", '6'),
+        ("seven", '7'),
+        ("eight", '8'),
+        ("nine", '9'),
+    ];
     let lines = input.lines();
-    lines.map(|line| {
-        let regexes = vec![
-            ("0", '0'),
-            ("1", '1'),
-            ("2", '2'),
-            ("3", '3'),
-            ("4", '4'),
-            ("5", '5'),
-            ("6", '6'),
-            ("7", '7'),
-            ("8", '8'),
-            ("9", '9'),
-            ("zero", '0'),
-            ("one", '1'),
-            ("two", '2'),
-            ("three", '3'),
-            ("four", '4'),
-            ("five", '5'),
-            ("six", '6'),
-            ("seven", '7'),
-            ("eight", '8'),
-            ("nine", '9'),
-        ];
-        let mut shortest: (Option<usize>, Option<char>) = (None, None);
-        let mut longest: (Option<usize>, Option<char>) = (None, None);
-        for regex in regexes {
-            let mat: Vec<_> = Regex::new(regex.0).unwrap().find_iter(&line).collect();
+    lines
+        .map(|line| {
+            let rev_line: String = line.chars().rev().collect();
+            let rev_matches: Vec<(String, char)> = regexes
+                .iter()
+                .map(|f| (f.0.chars().rev().collect(), f.1))
+                .collect();
 
-            match mat.iter().nth(0) {
-                Some(shortest_match) => match shortest.0 {
-                    Some(srt) => {
-                        if srt > shortest_match.start() {
-                            shortest = (Some(shortest_match.start()), Some(regex.1))
-                        }
-                    }
-                    None => shortest = (Some(shortest_match.start()), Some(regex.1)),
-                },
-                None => {}
-            }
-            match mat.iter().last() {
-                Some(longest_match) => match longest.0 {
-                    Some(long) => {
-                        if long <= longest_match.start() {
-                            longest = (Some(longest_match.start()), Some(regex.1))
-                        }
-                    }
-                    None => longest = (Some(longest_match.start()), Some(regex.1)),
-                },
-                None => {}
-            }
-        }
-        let out = format!("{}{}", shortest.1.unwrap(),  &longest.1.unwrap());
+            let out = format!(
+                "{}{}",
+                find_first(&regexes, line),
+                find_first(&rev_matches, &rev_line),
+            );
+            out.parse().unwrap()
+        })
+        .collect()
+}
+fn find_first<S: AsRef<str>>(options: &Vec<(S, char)>, haystack: &str) -> char {
+    let mut first_idx = 9999;
+    let mut char = None;
+    options
+        .iter()
+        .for_each(|f| {
+            if let Some(min) = haystack.find(f.0.as_ref()) {
 
-        out.parse().unwrap()
-    }).collect()
+            if min < first_idx {
+                first_idx = min;
+                char = Some(f.1)
+            }
+            }
+        });
+
+       char.unwrap()
 }
 
 #[cfg(test)]
@@ -82,5 +77,12 @@ zoneight234
             .to_string();
         let res: i64 = process(input).iter().sum();
         assert_eq!(281, res)
+    }
+
+    #[test]
+    fn more_input() {
+        let input = "oneight".to_string();
+        let res: i64 = process(input).iter().sum();
+        assert_eq!(18, res)
     }
 }
